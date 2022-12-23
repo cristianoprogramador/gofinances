4cb59e510273c4379d2ed7c4e5aa17ee83b06171
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-12-23 08:46:31 -0300

    Interface da primeira pagina finalizada

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,8 +1,10 @@
 export default {
   colors: {
     primary: "#5636D3",
+
     secondary: "#FF872C",
     secondary_light: "rgba(255,135,44,0.3)",
+
     success: "#12A454",
     success_light: "rgba(18,164,84,0.5)",
 
@@ -10,8 +12,10 @@ export default {
     attention_light: "rgba(232,63,91,0.5)",
 
     shape: "#FFFFFF",
+
     title: "#363F5F",
     text: "#969CB2",
+    text_dark: "#000000",
     background: "#F0F2F5",
   },
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Text } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
 import {
   Container,
   Header,
@@ -9,9 +15,45 @@ import {
   UserInfo,
   UserName,
   UserWrapper,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function DashBoard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$: 12.000,00",
+      category: { name: "Vendas", icon: "dollar-sign" },
+      date: "09/04/2022",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria Pizzy",
+      amount: "R$: 59,00",
+      category: { name: "Alimentação", icon: "coffee" },
+      date: "10/04/2022",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel do apartamento",
+      amount: "R$: 1.200,00",
+      category: { name: "Casa", icon: "home" },
+      date: "11/04/2022",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -25,8 +67,41 @@ export function DashBoard() {
               <UserName>Rodrigo</UserName>
             </User>
           </UserInfo>
+
+          <Icon name="power" />
         </UserWrapper>
       </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entrada"
+          amount="R$: 17.400,00"
+          lastTransaction="Última entrada dia 13 de setembro"
+        />
+        <HighlightCard
+          type="down"
+          title="Saidas"
+          amount="R$: 1.259,00"
+          lastTransaction="Última saida dia 03 de setembro"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$: 16.141,00"
+          lastTransaction="01 a 15 de setembro"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 }
