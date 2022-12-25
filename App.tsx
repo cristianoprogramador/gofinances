86543263a535d13d66fb868bf1b9fa83f19c8ed2
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-12-25 17:46:29 -0300

    Finalizado a parte visual

--- App.tsx
+++ App.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
-import { DashBoard } from "./src/screens/Dashboard";
-import * as SplashScreen from "expo-splash-screen";
 
 import {
-  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
+  useFonts,
 } from "@expo-google-fonts/poppins";
-import { Register } from "./src/screens/Register";
-import { CategorySelect } from "./src/screens/CategorySelect";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [canShow, setCanShow] = useState(false);
@@ -36,7 +34,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -20,6 +20,7 @@ import {
   Transactions,
   Title,
   TransactionList,
+  LogoutButton,
 } from "./styles";
 
 export interface DataListProps extends TransactionCardProps {
@@ -68,7 +69,9 @@ export function DashBoard() {
             </User>
           </UserInfo>
 
-          <Icon name="power" />
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
         </UserWrapper>
       </Header>
 
